7857a514fe30110672c6376dcb50039ad94dc196
k98788846-boop <k98788846@gmail.com>
2026-01-06 19:15:17 +0530

    ip 123456  op 456123

---
+++ Halfshift.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Halfshift{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+            a[i]=sc.nextInt();
+        for(int i=0;i<n;i++)
+            System.out.println(a[i]+" ");
+        int k=n/2;
+        for(int i=0;i<k;i++){
+            int temp=a[i];
+            a[i]=a[k+i];
+            a[k+i]=temp;
+        }
+        for(int i=0;i<n;i++)
+            System.out.println(a[i]+" ");
+    }
+}
