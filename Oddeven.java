ba04cc05b49ba1998aa652e37aab718650d14012
k98788846-boop <k98788846@gmail.com>
2026-01-06 17:13:56 +0530

    odd or even

---
+++ Oddeven.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Oddeven{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        if((a[i]%2)==0){
+            System.out.println(a[i] +"even number");
+
+        }else{
+            System.out.println(a[i]+"odd number");
+        }
+        }
+
+
+    }
+}
+
