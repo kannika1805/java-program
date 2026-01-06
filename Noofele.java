30d8e25c375243ae1e27da7d905b78c5c62757f8
k98788846-boop <k98788846@gmail.com>
2026-01-06 06:15:02 +0530

    number of elemnts

---
+++ Noofele.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Noofele{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+    System.out.println("number of element"+n);    }
+}
