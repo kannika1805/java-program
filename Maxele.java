e89f7034d094a67334913a16d7bf40c7e364922b
k98788846-boop <k98788846@gmail.com>
2026-01-06 18:24:04 +0530

    Secondhalfmin

--- Maxele.java
+++ Maxele.java
@@ -1,9 +1,9 @@
-import java.util.Scanner;
+ import java.util.Scanner;
 public class Maxele{ 
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);int n=sc.nextInt();
         int a[]=new int[n];int s=a[0];
-        for(int i=0;i<n;i++){           
+      for(int i=0;i<n;i++){           
             a[i]=sc.nextInt();
             if(s<=a[i]){
                 s=a[i];
---
+++ Secondhalfmin.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Secondhalfmin{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();}
+        for(int i=0;i<n;i++){
+            System.out.println(a[i]+" ");}
+        int k=(n/2)+1;
+        for(int i=0;i<k;i++){
+            int temp=a[k+i];
+            a[k+i]=a[n-1-i];
+            a[n-1-i]=temp;
+        }
+        for(int i=0;i<n;i++){
+            System.out.println(a[i]+" ");}
+
+
+    }
+}
