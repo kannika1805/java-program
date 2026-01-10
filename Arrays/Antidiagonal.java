33d45435c9edbfefd9f0065cab02b63895828ccc
k98788846-boop <k98788846@gmail.com>
2026-01-10 16:04:47 +0530

    dog

---
+++ Arrays/Antidiagonal.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Antidiagonal{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        int a[][]=new int[r][r];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<r;j++){
+                a[i][j]=sc.nextInt();
+            }
+        }for(int i=0;i<r;i++){
+            for(int j=0;j)
+        
+        }
+        }
+    }
+
