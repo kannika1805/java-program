4f16b32f8128cf0542d6ca2aa3f76f3379021148
k98788846-boop <k98788846@gmail.com>
2026-01-19 22:09:18 +0530

    no of vowels

---
+++ string/Vowels.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Vowels {
+public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    String s=sc.nextLine();int count=0;
+    for(int i=0;i<s.length();i++){
+        char ch=s.charAt(i);
+        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+               count++;
+        }
+
+    }
+    System.out.print("no of vowels"+count);
+    sc.close();
+
+}
+}
