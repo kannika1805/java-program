e8302ed584e67ef04d046f661286f87f52463438
k98788846-boop <k98788846@gmail.com>
2026-01-23 19:22:56 +0530

    replce all elment by 0 below diagonal

---
+++ Arrays/2darray/Lowertriangle.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Lowertriangle {
+ public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int r=sc.nextInt();
+    int a[][]=new int[r][r];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<r;j++){
+            a[i][j]=sc.nextInt();
+        }
+    }
+    for(int i=0;i<r;i++){
+        for(int j=0;j<r;j++){// elements above the diagonal i<j ,daigonal i=j,below diagonal i>j//
+            if(i>j){
+               a[i][j]=0; 
+            }
+        }
+    }
+    for(int i=0;i<r;i++){
+        for(int j=0;j<r;j++){
+            System.out.print(a[i][j]);
+        }
+    }
+    sc.close();
+}
+}
